45f9f9c61a77839f5eaa254f3df4540e1e3dffc4
samuel-witucki <samuel.witucki.p@gmail.com>
2021-02-28 13:39:47 -0600

    removed tensorflow example and added pytorch example

--- src/test.py
+++ src/test.py
@@ -1,5 +1,7 @@
 import cv2
-import tensorflow as tf
+import torch
+import math
+
 
 # uncomment this to test your webcam
 '''
@@ -20,39 +22,45 @@ while rval:
 cv2.destroyWindow("preview")
 '''
 
-# uncomment this to test tensorflow
+# uncomment this to test pytorch
 '''
-mnist =  tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+dtype = torch.float
+device = torch.device("cpu")
+# device = torch.device("cuda:0") # Uncomment this to run on GPU
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10)
-])
+# Create random input and output data
+x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
+y = torch.sin(x)
 
-predictions = model(x_train[:1]).numpy()
-predictions
+# Randomly initialize weights
+a = torch.randn((), device=device, dtype=dtype)
+b = torch.randn((), device=device, dtype=dtype)
+c = torch.randn((), device=device, dtype=dtype)
+d = torch.randn((), device=device, dtype=dtype)
 
-tf.nn.softmax(predictions).numpy()
+learning_rate = 1e-6
+for t in range(2000):
+    # Forward pass: compute predicted y
+    y_pred = a + b * x + c * x ** 2 + d * x ** 3
 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    # Compute and print loss
+    loss = (y_pred - y).pow(2).sum().item()
+    if t % 100 == 99:
+        print(t, loss)
 
-loss_fn(y_train[:1], predictions).numpy()
+    # Backprop to compute gradients of a, b, c, d with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_a = grad_y_pred.sum()
+    grad_b = (grad_y_pred * x).sum()
+    grad_c = (grad_y_pred * x ** 2).sum()
+    grad_d = (grad_y_pred * x ** 3).sum()
 
-model.compile(optimizer='adam',
-            loss=loss_fn,
-            metrics=['accuracy'])
+    # Update weights using gradient descent
+    a -= learning_rate * grad_a
+    b -= learning_rate * grad_b
+    c -= learning_rate * grad_c
+    d -= learning_rate * grad_d
 
-model.fit(x_train, y_train, epochs=5)
 
-model.evaluate(x_test, y_test, verbose=2)
-
-probability_model = tf.keras.Sequential([
-    model,
-    tf.keras.layers.Softmax()
-])
-probability_model(x_test[:5])
+print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
 '''
