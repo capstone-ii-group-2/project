7ba3a27464eb37173ed5592476a2ef7c098d49b9
samuel-witucki <samuel.witucki.p@gmail.com>
2021-03-25 14:23:58 -0500

    still very WIP but hopefully closer to training

---
+++ src/model.py
@@ -0,0 +1,28 @@
+from torch import nn
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net,self).__init__()
+
+        self.cnn_layers = nn.Sequential(
+            nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(4,4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+        )
+
+        self.linear_layers = nn.Sequential(
+            nn.Linear(4 * 7 * 7,10)
+        )
+
+
+    def forward(self, x):
+        x = self.cnn_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.linear_layers(x)
+        return x
--- src/neural_network.py
+++ src/neural_network.py
@@ -6,34 +6,98 @@ from torch import optim
 import torch.nn.functional as f
 from torchvision import datasets, transforms, models
 
-training_path = 'training_datasets/datasets/asl_alphabet_train'
-testing_path = 'training_datasets/datasets/asl_alphabet_test'
+def run():
+    training_path = 'training_datasets/datasets/asl_alphabet_train'
+    testing_path = 'training_datasets/datasets/asl_alphabet_test'
 
-test_size = 0.2
-batch_size = 32
-num_epoch = 10
-learning_rate = 0.001
-num_classes = 29
+    test_size = 0.2
+    batch_size = 32
+    num_epoch = 10
+    learning_rate = 0.001
+    num_classes = 29
 
-# define transformations for datasets
-train_transforms = transforms.Compose([
-    transforms.Resize(224),
-    transforms.ToTensor()
-])
+    # define transformations for datasets
+    train_transforms = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor()
+    ])
 
-test_transforms = transforms.Compose([
-    transforms.Resize(224),
-    transforms.ToTensor()
-])
+    test_transforms = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor()
+    ])
 
-# load datasets
-train_dataset = datasets.ImageFolder(training_path, transform=train_transforms)
-num_train = len(train_dataset)
-print(train_dataset)
+    # load datasets
+    train_dataset = datasets.ImageFolder(training_path, transform=train_transforms)
+    test_dataset = datasets.ImageFolder(testing_path, transform=test_transforms)
+    num_train = len(train_dataset)
+    print(train_dataset)
 
-# define train dataset loader
-train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset,batch_size=batch_size,shuffle=True,num_workers=4)
-classes = train_dataloader.dataset.classes
-print(classes)
+    # define train dataset loader
+    train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset,batch_size=batch_size,shuffle=True,num_workers=4)
+    classes = train_dataloader.dataset.classes
+    test_dataloader = torch.utils.data.DataLoader(dataset=test_dataset,batch_size=batch_size)
+    print(classes)
+    print(len(classes))
+    return
+    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu')
+    model = models.resnet50(pretrained=True)
+    for param in model.parameters():
+            param.requires_grad = False
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.fc = nn.Sequential(nn.Linear(2048, 512), nn.ReLU(), nn.Dropout(0.2), nn.Linear(512,10), nn.LogSoftmax(dim=1))
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
+    model.to(device)
+    print(model)
+
+    epochs = 1
+    steps = 0
+    running_loss = 0
+    print_every = 10
+    train_losses, test_losses = [],[]
+
+    for epoch in range(num_epoch):
+        for inputs, labels in train_dataloader:
+            steps +=1
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                test_loss =0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in test_dataloader:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+                        test_loss += batch_loss.item()
+
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1,dim=1)
+                        equals = top_class==labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                        train_losses.append(running_loss/len(train_dataloader))
+                        test_losses.append(test_loss/len(test_dataloader))
+                        print(f"Epoch {epoch + 1}/{epochs}.. "
+                              f"Train loss: {running_loss / print_every:.3f}.. "
+                              f"Test loss: {test_loss / len(test_dataloader):.3f}.. "
+                              f"Test accuracy: {accuracy / len(test_dataloader):.3f}")
+                        running_loss = 0
+                        model.train()
+    torch.save(model, 'testmodel.pth')
+
+    plt.plot(train_losses, label='Training loss')
+    plt.plot(test_losses, label='Validation loss')
+    plt.legend(frameon=False)
+    plt.show()
+
+if __name__=="__main__":
+    run()
